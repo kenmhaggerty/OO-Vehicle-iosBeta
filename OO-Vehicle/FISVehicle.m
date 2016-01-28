db1b4f495bd3d3cc5a034e2b8e286b8e5bf19b4f
kenmhaggerty <kenmhaggerty@gmail.com>
2016-01-28 17:20:06 -0500

    Creates FISVehicle and stubs out methods.

---
+++ OO-Vehicle/FISVehicle.m
@@ -0,0 +1,45 @@
+//
+//  FISVehicle.m
+//  OO-Vehicle
+//
+//  Created by Ken M. Haggerty on 1/28/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "FISVehicle.h"
+
+@implementation FISVehicle
+
+- (instancetype)init {
+    
+    self = [super init];
+    return self;
+}
+
+- (instancetype)initWithWeight:(CGFloat)weight topSpeed:(CGFloat)topSpeed {
+    
+    self = [super init];
+    return self;
+}
+
+- (void)increaseSpeed {
+    
+    //
+}
+
+- (void)brake {
+    
+    //
+}
+
+- (void)turnLeft {
+    
+    //
+}
+
+- (void)turnRight {
+    
+    //
+}
+
+@end
